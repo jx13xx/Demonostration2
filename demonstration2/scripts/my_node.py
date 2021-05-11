c12ad1580d20c6b40b34402652588be18a6ab4f8
jx13xx <jeenxavier23@gmail.com>
2021-05-12 03:57:39 +0400

    added and changed the file

--- demonstration2/scripts/my_node.py
+++ demonstration2/scripts/my_node.py
@@ -11,25 +11,39 @@
     Created: 2021/04/08
     Author: Brendan Halloran
 """
-
+import numpy as np
 import rospy
+import cv2
 import time
 from nav_msgs.msg import OccupancyGrid, Odometry, Path
 from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import MarkerArray
 from std_srvs.srv import SetBool
+import imutils
 from demonstration2.msg import custom
+from sensor_msgs.msg import CompressedImage, Image
+from cv_bridge import CvBridge, CvBridgeError
+
 from datetime import datetime
 
 class my_node:
     def __init__(self):
+        # defining parameters for OpenCV image
+        self.bridge = CvBridge()
+        self.colour_frame = None
+        self.depth_frame = None
+        self.num_colour_images = 0
+        self.num_depth_images = 0
+        self.subscriber_colour = rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.callback_colour)
+        self.subscriber_depth = rospy.Subscriber('/camera/depth/image_raw', Image, self.callback_depth)
+
         # Subs and pubs
         self.subscriber_frontiers = rospy.Subscriber('/explore/frontiers', MarkerArray, self.callback_frontiers)
         self.subscriber_map = rospy.Subscriber('/map', OccupancyGrid, self.callback_map)
         self.subscriber_odom = rospy.Subscriber('/odom', Odometry, self.callback_odom)
-        self.publisher_move_base_simple_goal = rospy.Publisher('move_base_simple/goal/', PoseStamped, queue_size=1)
         self.publisher_map = rospy.Publisher('/ecte477/map', OccupancyGrid, queue_size=1)
         self.publisher_path = rospy.Publisher('/ecte477/path', Path, queue_size=1)
+        self.publisher_move_base_simple_goal = rospy.Publisher('move_base_simple/goal/', PoseStamped, queue_size=1)
         #load the beacon file
         self.beacons = rospy.get_param("beacons")
         #publish the beacon
@@ -44,7 +58,7 @@ class my_node:
         rospy.wait_for_service('explore/explore_service')
         start_explore_lite = rospy.ServiceProxy('explore/explore_service', SetBool)
         resp  = start_explore_lite(True)
-        test = "dfsfsdASDSAD"
+
 
 
         for x in self.beacons:
@@ -67,7 +81,25 @@ class my_node:
     # Simply repeact the map data to the correct topic
     def callback_map(self, data):
         self.publisher_map.publish(data);
-        
+
+    def callback_colour(self, colour_image):
+        rospy.loginfo('[Image Processing] callback_colour')
+        np_array =  np.fromstring(colour_image.data, np.uint8)
+
+        blurred = cv2.GaussianBlur(self.colour_frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        self.colour_frame = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
+        colour_lower = (89, 255, 255)
+        colour_upper = (36, 50, 70)
+        mask = cv2.inRange(hsv, colour_lower, colour_upper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        cv2.imshow('Masked Image', mask)
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+
+
+
     # Turn the odometry info into a path and repeat it to the correct topic
     def callback_odom(self, data):
         pose = PoseStamped()
@@ -83,6 +115,7 @@ class my_node:
             goal_msg.header.frame_id = 'map'
             goal_msg.pose.orientation.w = 1.0;
             self.publisher_move_base_simple_goal.publish(goal_msg)
+            rospy.loginfo("Exploration Done")
 	
 	
 	
